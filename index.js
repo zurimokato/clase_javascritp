d5645ffa7a3e5e32ea7f608e70f200eac83e7a13
zurimokato <noj2304@gmail.com>
2023-05-13 10:20:06 -0500

    variables

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-alert('Hola Mundo')
+//alert('Hola Mundo');
+
+//comentario de una linea
+
+/*comentario de varias 
+    lineas 
+    comentando
+
+*/
+
+//alert("hola"); alert("Mundo");
+
+
+//variables
+let a=5;
+let b=6;
+
+let suma=a+b;
+console.log('El resultado de la suma es :'+suma);
+
+let nombre='Jhoan';
+let nombre_dos =nombre;
+
+nombre_dos="Jesus";
+
+console.log("El prmer nombre es :"+ nombre);
+console.log("E segundo nombre es:"+ nombre_dos);
+
+//Notaciones de variables
+calmelCase=5;
+snake_case=5;
+Pascal_Cae=5;
+
+let nombre_competo ="Jhoan Olivo Barrios";
+
+//no puedes iniciar con número
+//no puede tener guión medio
+
+
+
+//constantes
+const PI=3.1416;
+const COLOR="ROJO";
